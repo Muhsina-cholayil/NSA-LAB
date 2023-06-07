48155a5f690ca192347197c1b5c58c43802b81da
Muhsina-cholayil <116860571+Muhsina-cholayil@users.noreply.github.com>
2023-06-07 02:26:20 -0700

    Add files via upload

---
+++ arithmetic.sh
@@ -0,0 +1,14 @@
+echo "enter first number:"
+read a
+echo "enter second number:"
+read b
+sum=$(( a + b ))
+dif=$(( a - b ))
+mul=$(( a * b ))
+div=$(( a / b ))
+echo "sum:" $sum
+echo "difference:" $dif
+echo "multiplication:" $mul
+echo "division:" $div
+
+~
