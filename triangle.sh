3332fb71ab4458b711c13370ac15e585477d124e
Muhsina-cholayil <116860571+Muhsina-cholayil@users.noreply.github.com>
2023-06-14 14:48:44 +0530

    Add files via upload

--- triangle.sh
+++ triangle.sh
@@ -7,7 +7,13 @@ read c
 
 if ((a+b > c && a+c > b && b+c >a))
 then
-echo "sides can form triangle"
+   if [[ $((a*a)) -eq $((b*b + c*c)) || $((b*b)) -eq $((a*a + b*b)) || $((c*c)) -eq $((a*a + b*b)) ]]
+   then
+   echo "can form right angled triangle"
+   else
+   echo "cannot form right angled triangle"
+   fi
+
 else
 echo "sides cannot form triangle"
 fi
